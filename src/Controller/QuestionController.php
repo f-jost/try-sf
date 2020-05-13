17a9b8ffae901571861add76145abb6985df1f76
LeFredd <frederic@les-tilleuls.coop>
2020-05-14 01:17:34 +0200

    feat: write in a twig template

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -22,6 +23,15 @@ class QuestionController
      */
     public function show(string $slug): Response
     {
-        return new Response(ucwords(str_replace('-', ' ', $slug)));
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers,
+        ]);
     }
 }
